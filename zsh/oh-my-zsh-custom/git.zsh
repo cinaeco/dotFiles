b34f9014747ad9f4916de1e280bc4f6d676af73b
Weiyi Lou <cinaeco@yahoo.com>
2012-07-27 21:34:43 +1000

    add more git shortcuts

--- zsh/oh-my-zsh-custom/git.zsh
+++ zsh/oh-my-zsh-custom/git.zsh
@@ -1,13 +1,24 @@
 ## preferred git shortcuts not in the git plugin
+#
 alias ga.='git add .'
 alias gs='git status'
 alias gd='git diff'
 alias gds='git diff --staged'
+
+alias gcob='git checkout -b'
+alias gf='git fetch'
+
+alias gbd='git branch -d'
+alias gbD='git branch -D'
+
 alias gl='git log --graph --date=relative --pretty="format:%C(yellow)%h %Cblue[%cn] %Creset%s... %cd"'
 alias gls='git log --graph --stat'
 alias glp='git log -p'
+
 alias gr='git rebase'
+
 alias gtrack='git branch --set-upstream $(current_branch) origin/$(current_branch)'
+
 # useful git plugin built-in ones include:
 #   ga, gc, gco, gb, gba, gm, grhh, ggpull, ggpush
 # ggpull translates into `git pull origin <current branch>`, same for ggpush
