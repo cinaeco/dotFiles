2430f884152e908ca9abc51bfcad25ebf748b438
weiyi <lou@acer.edu.au>
2015-02-19 16:19:56 +1100

    Add and remove various vim plugins, move ssh auth socket to fixed place
    Moving the socket ensures that the socket location in tmux shell environments
    does not get stale, and so lose our ssh agent forwarding.

--- zsh/omz-custom/ssh-add.zsh
+++ zsh/omz-custom/ssh-add.zsh
@@ -6,3 +6,14 @@
 if [[ `ssh-add -L | grep "^ssh-" | wc -l` -eq 0 ]]; then
   ssh-add 2> /dev/null
 fi
+
+# Put ssh authentication socket in predicatable place for ssh-agent forwarding.
+#
+# This allows us to overcome stale sockets in tmux.
+SOCK="/tmp/ssh-agent-$USER-tmux"
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+then
+    rm -f /tmp/ssh-agent-$USER-screen
+    ln -sf $SSH_AUTH_SOCK $SOCK
+    export SSH_AUTH_SOCK=$SOCK
+fi
