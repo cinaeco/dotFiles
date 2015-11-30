2621fe3f0bcacd3a2b157f7f8545bf5cdec834a1
Weiyi Lou <cinaeco@gmail.com>
2015-11-30 17:04:39 +1100

    Add `cu` alias and completion
    `cu` can be used in any terminal emulator to talk to serial
    devices/consoles/ports, instead of using a dedicated console application
    like ZTerm. This is useful for managing cisco devices, for example.
    
    Also got to learn about the zsh completion system. Now, any custom
    completions that we do not want to turn into a plugin can be put in
    `zsh/custom/completions`.

--- zsh/custom/aliases-misc.zsh
+++ zsh/custom/aliases-misc.zsh
@@ -4,6 +4,10 @@ alias hosts='sudo vim /etc/hosts'
 # Common places
 alias cd.='cd ~/dotfiles'
 
+# Serial console access ("call up line"). Type `~.` (and wait) to disconnect.
+alias cul='sudo cu -s 9600 -l'
+compdef _cu cul
+
 # Folder listing/traversal, least to most detailed
 alias l='ls'
 alias la='ls -A'
