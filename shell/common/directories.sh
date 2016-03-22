7e1cc806e65ae7dbb510f4030302c932742d6a88
Weiyi Lou <cinaeco@gmail.com>
2016-03-22 20:37:57 +1100

    Add coloured output for ls

--- shell/common/directories.sh
+++ shell/common/directories.sh
@@ -1,3 +1,11 @@
+# Coloured output for ls
+if [[ -n $(command -v dircolors) ]]; then
+  eval "`dircolors -b`"
+  alias ls='ls --color=auto'
+elif [[ $(uname -s) = Darwin ]]; then
+  alias ls='ls -G'
+fi
+
 # Folder listing/traversal
 alias l='ls'
 alias la='ls -A'
