93b71001b034ab89d3b385c4ac48dce15fb4b948
Weiyi Lou <cinaeco@gmail.com>
2016-03-23 11:28:55 +1100

    Add bash-completion loading

--- shell/bash/completion.bash
+++ shell/bash/completion.bash
@@ -1,2 +1,11 @@
 bind "set completion-ignore-case on"
 bind "set show-all-if-ambiguous on"
+
+# Bash completion (https://github.com/scop/bash-completion).
+if [[ -f /usr/share/bash-completion/bash_completion ]]; then
+  source /usr/share/bash-completion/bash_completion
+elif [[ -f /etc/bash_completion ]]; then
+  source /etc/bash_completion
+elif [[ -n $(command -v brew) && -f $(brew --prefix)/etc/bash_completion ]]; then
+  source $(brew --prefix)/etc/bash_completion
+fi
