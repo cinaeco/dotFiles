4b9ae8bfcead2d8c2cf9dca8462cdfaf8e5e0c23
Weiyi Lou <cinaeco@gmail.com>
2016-03-24 15:07:01 +1100

    Move less options into `less.sh`

--- shell/common/less.sh
+++ shell/common/less.sh
@@ -1,3 +1,11 @@
+# Default Flags for `less`
+# e = quit at end of file
+# i = searches ignore case, unless there are upper case characters
+# r = display raw control characters (Ctrl-A is ^A)
+# M = verbose `less` prompt
+# X = disable termcap init/deinit strings - sometimes these clear the screen.
+export LESS='-iRMX'
+
 if srchilitepath=$(command -v src-hilite-lesspipe.sh); then
   export LESSOPEN="| $srchilitepath %s"
 fi
