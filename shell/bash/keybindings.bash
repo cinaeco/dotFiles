0a35aa2bbc6b91f7bdb1926a0c247fff89dfd4af
Weiyi Lou <cinaeco@gmail.com>
2016-04-16 17:31:47 +1000

    Add back ctrl-l to clear screen in bash
    Removed when we switched off `bind-tty-special-chars`.

--- shell/bash/keybindings.bash
+++ shell/bash/keybindings.bash
@@ -1,6 +1,12 @@
 # Use vi-bindings in readline for command editing
 set -o vi
 
-# Set Ctrl-w to delete by word boundaries instead of to the previous space.
+# Stop readline from auto-binding some control characters (e.g. ctrl-w) so that
+# they can be reassigned as desired.
 bind 'set bind-tty-special-chars off'
+
+# Set Ctrl-w to delete by word boundaries instead of to the previous space.
 bind '\C-w:backward-kill-word'
+
+# Set Ctrl-l to clear the screen.
+bind '\C-l:clear-screen'
